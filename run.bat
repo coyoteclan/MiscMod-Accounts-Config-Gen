c899f9302691dde1848755e2eed2a2d9ffee4ffc
Mechamorph <118144070+coyoteclan@users.noreply.github.com>
2024-04-12 10:20:37 +0500

    Specify pysimplegui version
    The newer version requires license for both personal and commercial use.

--- run.bat
+++ run.bat
@@ -3,7 +3,7 @@ REM Update Python and pip
 python -m pip install --upgrade pip setuptools wheel
 
 REM Install the required Python packages
-pip install pysimplegui
+pip install pysimplegui==4.60.5
 
 REM Run the Python script
 python main.py
